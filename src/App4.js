5277ccd38ce72355d925fb818af1a28cf7359519
085 - Haris Mashhood <harismashhood36@gmail.com>
2022-09-16 21:05:11 +0000

    Update App3.js and index.js

---
+++ src/App4.js
@@ -0,0 +1,39 @@
+import React, {useState,useEffect} from 'react'
+
+
+
+
+
+
+
+
+const App4 = () => {
+    const [size,setSize] = useState(window.screen.width);
+    const actualWidth = ()=>{
+        setSize(window.innerWidth);
+        console.log(window.innerWidth);
+    }
+    useEffect(()=>{
+        window.addEventListener('resize',actualWidth);
+
+
+
+    },[size])
+  return (
+    <div>
+
+
+
+     <h2>Screen size is</h2>
+
+
+
+     <h2>{size}px</h2>
+
+
+
+    </div>
+  )
+}
+
+export default App4
